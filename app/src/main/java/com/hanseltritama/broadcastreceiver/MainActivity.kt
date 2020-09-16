044526ed0aa975235c420bf7f9361d77ee66aaf4
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2020-09-16 16:52:51 -0700

    register & unregister broadcastreceiver on Activity lifecycle

--- app/src/main/java/com/hanseltritama/broadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/hanseltritama/broadcastreceiver/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.hanseltritama.broadcastreceiver
 
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    val myBroadcastReceiver: MyBroadcastReceiver = MyBroadcastReceiver()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    // when app is on the foreground
+    override fun onStart() {
+        super.onStart()
+        val intent: IntentFilter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
+        registerReceiver(myBroadcastReceiver, intent)
+    }
+
+    // when app is on the background
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(myBroadcastReceiver)
     }
 }
